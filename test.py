e1540acf92b9659513563adcd24544865d45f421
Artem Vesnin <artem_vesnin@iszf.irk.ru>
2026-02-10 08:54:27 +0000

    Added test for zero devision case (exception must be thorwed)

--- test.py
+++ test.py
@@ -14,7 +14,17 @@ def test_division():
     assert divide(a, b) == result
     print("Test division PASSED")
 
+def test_zero_division():
+    a = 5
+    b = 0
+    try:
+        divide(a, b)
+        print("Test zero-division FAILED")
+        assert False
+    except ValueError as e:
+        print("Test zero-division PASSED")
 
 if __name__ == "__main__":
     test_addition()
     test_division()
+    test_zero_division()
