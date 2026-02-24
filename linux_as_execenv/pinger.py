549a9c7489546645a6d92bacdeab41d7aa12cf6d
Artem Vesnin <artem_vesnin@iszf.irk.ru>
2026-02-24 08:39:33 +0000

    Added prommatic date definition to work with most recent date

--- linux_as_execenv/pinger.py
+++ linux_as_execenv/pinger.py
@@ -1,7 +1,13 @@
 import requests
+from datetime import datetime, timedelta
 
 url_template = "https://simurg.space/gen_file?data=obs&date={date}"
-url = url_template.format(date="2026-02-20")
+
+now = datetime.now()
+date_str = now.strftime("%Y-%m-%d") 
+print(f"Current datetime is {now} and date {date_str}")
+
+url = url_template.format(date=date_str)
 
 response = requests.get(url=url, stream=True)
 print(f"From url {url} got {response}")
