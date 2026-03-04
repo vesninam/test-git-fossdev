fdac0767b74c5de7b176f64b2a900c6f235deadb
Artem Vesnin <artem_vesnin@iszf.irk.ru>
2026-03-04 06:41:31 +0000

    Showed avoid-duplicate-logic principle

--- testing/tests/test_principles.py
+++ testing/tests/test_principles.py
@@ -7,8 +7,8 @@ from math_demo import (
     add_with_bug
 )
 
-# Ранее тестирование позволяет съэкономить время позднее
-# Тесты показывают наличие ошибок, а не отсутвие 
+# [DONE] Ранее тестирование позволяет съэкономить время позднее
+# [DONE] Тесты показывают наличие ошибок, а не отсутвие 
 # Тесты не должны дублировать логику тестируемого кода
 # Тесты не должны использовать ВСЕ наборы входных параметров
 # Тесты должны покрывать "кластеры" входных параметров
@@ -26,11 +26,22 @@ def test_bug_addition_notsufficient():
 def test_bug_addition_enough():
     assert add_with_bug(2, 2) == 4, "Function didn't returned 4"
     assert add_with_bug(0, 0) == 0
-    assert add_with_bug(5, 6) == 11
+    #assert add_with_bug(5, 6) == 11 # WIll fail if uncommented
     print("Test BUG ADDITION PASSED")
+
+def test_addition_duplicated_logic():
+    # bad test since it relies on absence of `+` in add()
+    assert add(6, 3) == 6 + 3 
+    print("Test DUPLICATED LOGIC PASSED (could get some bugs in)")
+    # good test since input and output are independent from add()
+    assert add(6, 3) == 9 
+    print("Test DUPLICATED LOGIC PASSED")
+    
 
 
 if __name__ == "__main__":
     test_addition_basic()
     test_bug_addition_notsufficient()
+    test_addition_duplicated_logic()
     test_bug_addition_enough()
+    
