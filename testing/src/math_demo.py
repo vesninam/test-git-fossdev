779ec878dd68fe654d7ec9fe2e0162e7e47b4a8b
Artem Vesnin <artem_vesnin@iszf.irk.ru>
2026-03-04 06:30:46 +0000

    Show that tests not always show a bug

--- testing/src/math_demo.py
+++ testing/src/math_demo.py
@@ -1,2 +1,6 @@
 def add(a, b):
     return a + b
+
+
+def add_with_bug(a, b):
+    return a * b
--- testing/tests/test_principles.py
+++ testing/tests/test_principles.py
@@ -2,7 +2,10 @@ import sys
 # TODO make with `pip install -e .`
 sys.path.append("../src")
 
-from math_demo import add
+from math_demo import (
+    add,
+    add_with_bug
+)
 
 # Ранее тестирование позволяет съэкономить время позднее
 # Тесты показывают наличие ошибок, а не отсутвие 
@@ -13,8 +16,21 @@ from math_demo import add
 # Тесты покрывают как успешные так и ошибочные кейсы
 
 def test_addition_basic():
-    assert add(2, 2) == 4
+    assert add(2, 2) == 4, "Function didn't returned 4"
     print("Test BASIC ADDITION PASSED")
+
+def test_bug_addition_notsufficient():
+    assert add_with_bug(2, 2) == 4, "Function didn't returned 4"
+    print("Test BUG ADDITION PASSED")
+
+def test_bug_addition_enough():
+    assert add_with_bug(2, 2) == 4, "Function didn't returned 4"
+    assert add_with_bug(0, 0) == 0
+    assert add_with_bug(5, 6) == 11
+    print("Test BUG ADDITION PASSED")
+
 
 if __name__ == "__main__":
     test_addition_basic()
+    test_bug_addition_notsufficient()
+    test_bug_addition_enough()
