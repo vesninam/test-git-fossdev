f6e98a0b14329d7e9b2db577340e40d230a8584e
Artem Vesnin <artem_vesnin@iszf.irk.ru>
2026-04-01 05:00:25 +0000

    Added fields to setup to remove packging warnings

--- testing_tdd/setup.py
+++ testing_tdd/setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "ndfl",
+    name = "ndfl-avm",
     version = "0.0.0",
-
+    long_description = "Tax calculator",
+    long_description_content_type = "text/markdown",
     package_dir = {"": "src"},
-    packages = find_packages(where="src")
+    packages = find_packages(where="src"),
+    author='Artem Vesnin',
+    author_email='artemvesnin@gmail.com',
+
 )
